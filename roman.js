cd5a090e7d39cf653f16c2b2eb737cafdaafaca0
Kevin(Jiahong) Zheng <kvinzheng@gmail.com>
2017-07-12 13:48:42 -0700

    updated roman

--- roman.js
+++ roman.js
@@ -1,3 +1,21 @@
 function roman(string){
-  string.split('')
+  let sum = 0;
+  let hash = {
+    'I' : 1,
+    'V' : 5,
+    'X' : 10,
+    'L' : 50,
+  };
+
+  let arr = string.split('');
+  for(let i = 0; i < arr.length; i++) {
+    if(hash[arr[i]] < hash[arr[i+1]]){
+      sum -= hash[arr[i]];
+    } else {
+      sum += hash[arr[i]];
+    }
+  }
+  return sum;
 }
+
+module.exports = roman;
